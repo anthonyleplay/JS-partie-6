69f0cd6075997b42df191b5a9f2de6ef171d3e0d
Anthony Le Play <anthony.leplay@gmail.com>
2020-04-29 14:54:02 +0200

    exo3 fini

--- exo3/assets/js/script.js
+++ exo3/assets/js/script.js
@@ -1,1 +1,12 @@
-let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
+let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++', 'ruby', 'python'];
+
+let idList = document.getElementById("list");
+
+var text = "";
+var i;
+for (i = 0; i < languages.length; i++) {
+
+    text += i+1 + " : " + languages[i] + "<br>";
+
+}
+idList.innerHTML = text;
